6f1b5e2c6c6c3fb9aa67d52ff7f9d6c23137eb9f
nichd147 <diniig147@gmail.com>
2025-01-19 23:13:25 +0400

    adds Droncontroller java app

---
+++ upd_server/droncontroller/src/main/java/sir/andrusha/droncontroller/DroncontrollerApplication.java
@@ -0,0 +1,13 @@
+package sir.andrusha.droncontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroncontrollerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroncontrollerApplication.class, args);
+	}
+
+}
