ea199664ff83cd9c28eec22db2ddfc07906670a4
nichd147 <diniig147@gmail.com>
2025-01-20 00:15:19 +0400

    implements ping-pong in java

--- upd_server/droncontroller/src/main/java/sir/andrusha/droncontroller/DroncontrollerApplication.java
+++ upd_server/droncontroller/src/main/java/sir/andrusha/droncontroller/DroncontrollerApplication.java
@@ -3,11 +3,17 @@ package sir.andrusha.droncontroller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import java.io.IOException;
+
+//@SpringBootApplication
 public class DroncontrollerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DroncontrollerApplication.class, args);
+//	public static void main(String[] args) {
+//		SpringApplication.run(DroncontrollerApplication.class, args);
+//	}
+
+	public static void main(String[] args) throws IOException {
+		new UdpServer().start();
 	}
 
 }
